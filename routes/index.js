3ef0de3a536f4fc70daf3e8340bef5582dcd7ac7
devitasari <devitas700@gmail.com>
2019-11-01 11:53:40 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
-const route = require('express').Router()
+const router = require('express').Router()
+const UserController = require('../controllers/user')
+const verify = require('../middleware/googleVerify')
+const {authentication}  = require('../middleware/auth')
+
+router.get('/users/test', authentication, UserController.coba)
+router.post('/users/googleSignIn', verify, UserController.googleSignIn)
+router.post('/users/manualSignUp', UserController.manualSignUp)
+router.post('/users/manualSignIn', UserController.manualSignIn)
 
 
-module.exports = route
+module.exports = router
